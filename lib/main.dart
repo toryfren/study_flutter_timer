be1e63027f095cbd6d6cdd650e83380c01f2b076
Ted.seo <ted.seo@playxtechnology.com>
2020-07-30 09:44:29 +0900

    actions && BlocBuilder에 액션 랜더링 추가.

--- lib/main.dart
+++ lib/main.dart
@@ -49,17 +49,84 @@ class Timer extends StatelessWidget {
                       .floor()
                       .toString()
                       .padLeft(2, '0');
-                  final String secondsStr = (state.duration % 60).floor().toString().padLeft(2, '0');
+                  final String secondsStr =
+                      (state.duration % 60).floor().toString().padLeft(2, '0');
                   return Text(
                     '$minutesStr:$secondsStr',
                     style: Timer.timerTextStyle,
                   );
-                }
-              )
-            )
-          )
+                },
+              ),
+            ),
+          ),
+          BlocBuilder<TimerBloc, TimerState>(
+            buildWhen: (previousState, state) =>
+                state.runtimeType != previousState.runtimeType,
+            builder: (context, state) => Actions(),
+          ),
         ],
       ),
     );
+  }
+}
+
+class Actions extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: _mapStateToActionButtons(
+        timerBloc: BlocProvider.of<TimerBloc>(context),
+      ),
+    );
+  }
+
+  List<Widget> _mapStateToActionButtons({
+    TimerBloc timerBloc,
+  }) {
+    final TimerState currentState = timerBloc.state;
+    if (currentState is TimerInitial) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () =>
+              timerBloc.add(TimerStarted(duration: currentState.duration)),
+        ),
+      ];
+    }
+    if (currentState is TimerRunInProgress) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.pause),
+          onPressed: () => timerBloc.add(TimerPaused()),
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.replay),
+          onPressed: () => timerBloc.add(TimerReset()),
+        ),
+      ];
+    }
+    if (currentState is TimerRunPause) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () => timerBloc.add(TimerResumed()),
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.replay),
+          onPressed: () => timerBloc.add(TimerReset()),
+        ),
+      ];
+    }
+    if (currentState is TimerRunComplete) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.replay),
+          onPressed: () => timerBloc.add(TimerReset()),
+        ),
+      ];
+    }
+    return [];
   }
 }
